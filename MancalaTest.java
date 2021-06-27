dfb1712f0f2dd2223b05de41fe4999e029d8b6ea
anant7ue <anant7ue@gmail.com>
2021-06-27 13:42:16 +0530

    Update MancalaTest.java

--- MancalaTest.java
+++ MancalaTest.java
@@ -4,6 +4,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import com.jsample.testcategories.*;
 
 import static org.junit.Assert.*;
 
