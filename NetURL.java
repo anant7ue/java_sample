1c0b2c6b46d3b859374d11fc61762c0efd4e73c1
anant7ue <anant7ue@gmail.com>
2021-08-25 11:50:09 +0530

    Create NetURL.java

---
+++ NetURL.java
@@ -0,0 +1,17 @@
+
+import java.net.*;
+import java.io.*;
+
+public class NetUrl {
+    public static void main(String[] args) throws MalformedURLException {
+        System.out.println("hello from java net url");
+        String i4 = "https://80.45.9.1:443";
+        String i6 = "https://[fe80:a4b5::1]:443";
+        URL url4 = new URL(i4);
+        URL url6 = new URL(i6);
+        System.out.println("java net port4 =" + url4.getPort());
+        System.out.println("java net host4 = " + url4.getHost());
+        System.out.println("java net port6 = " + url6.getPort());
+        System.out.println("java net host6 = " + url6.getHost());
+    }
+}
