6526fa945f0fdee2fb2a250d2f7e93b55f0a0ae1
anant7ue <anant7ue@gmail.com>
2021-08-30 22:33:16 +0530

    Update NetURL.java

--- NetURL.java
+++ NetURL.java
@@ -3,6 +3,7 @@ import java.net.*;
 import java.io.*;
 
 public class NetUrl {
+    
     public static void main(String[] args) throws MalformedURLException {
         System.out.println("hello from java net url");
         String i4 = "https://80.45.9.1:443";
@@ -13,5 +14,16 @@ public class NetUrl {
         System.out.println("java net host4 = " + url4.getHost());
         System.out.println("java net port6 = " + url6.getPort());
         System.out.println("java net host6 = " + url6.getHost());
+        
+        String ipV6Pattern = "\\[([a-zA-Z0-9:]+)\\]:(\\d+)";
+        String ipV4Pattern = "([a-zA-Z0-9.]+)\\:(\\d+)";
+        Pattern p6 = Pattern.compile( ipV6Pattern );
+        Matcher m6 = p6.matcher( ip );
+        Pattern p4 = Pattern.compile( ipV4Pattern );
+        Matcher m4 = p4.matcher( ip );
+        
+        if( m4.matches() || m6.matches()) {
+        }
+
     }
 }
